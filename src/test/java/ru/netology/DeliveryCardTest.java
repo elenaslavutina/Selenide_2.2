04270d208a53e68d8ed27783d82008517014f9c4
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-07 20:37:42 +0300

    test failed

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -32,21 +34,25 @@ class DeliveryCardTest {
         driver.quit();
         driver = null;
     }
-  /*  @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[name='number']").setValue("4055 0100 0123 4613 8564");
-        $("[name='phone']").setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).waitUntil(visible, 5000);
-        $(byText("Личный кабинет")).waitUntil(visible, 5000);
-    }
 
     @Test
+    void shouldRegisterByAccountNumberDOMModification() {
+        open("http://localhost:9999");
+        //  $$(".tab-item").find(exactText("По номеру счёта")).click();
+        $("[data-test-id='city']").setValue("Казань");
+        $("[data-test-id = 'date']").setValue("2020-06-20");
+        $("[data-test-id= 'name']").setValue("Василий Пупкин");
+        $("[data-test-id= 'phone']").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 5000);
+        //$(byText("Личный кабинет")).waitUntil(visible, 5000);
+    }
+
+/*    @Test
     void shouldRegisterByAccountNumberVisibilityChange() {
         open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
+    //    $$(".tab-item").find(exactText("По номеру счёта")).click();
         $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
         $$("[name='phone']").last().setValue("+792000000000");
         $$("button").find(exactText("Продолжить")).click();
@@ -54,6 +60,6 @@ class DeliveryCardTest {
         $(byText("Личный кабинет")).waitUntil(visible, 5000);
     }
 
-   */
+*/
 }
 
