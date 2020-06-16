be2be8860f0d24a718bcc5b4c0bad308b361fb0e
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-16 17:40:26 +0300

    problems with data format

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -7,45 +7,35 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeliveryCardTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
         open("http://localhost:9999");
-        //  $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[data-test-id='city']").setValue("Казань");
-        $("[data-test-id = 'date']").setValue("2020-06-20");
-        $("[data-test-id= 'name']").setValue("Василий Пупкин");
-        $("[data-test-id= 'phone']").setValue("+79270000000");
+
+
+        $("[data-test-id= city] input").setValue("Казань");
+        $("[data-test-id= date] input").sendKeys("25.06.2020");
+
+      $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
+
+        $("[data-test-id=phone] input").sendKeys("+71231234123");
+
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).waitUntil(visible, 5000);
+        $(withText("Успешно!")).waitUntil(visible, 15000);
         //$(byText("Личный кабинет")).waitUntil(visible, 5000);
     }
 
