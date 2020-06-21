96ec48f008bc429ccfb349f31e3cc95433e60023
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-21 20:12:14 +0300

    code aline

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -1,10 +1,13 @@
 package ru.netology;
+
 import ru.netology.CityCreater;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static com.codeborne.selenide.Condition.*;
@@ -15,32 +18,24 @@ import static org.openqa.selenium.By.cssSelector;
 
 class DeliveryCardTest {
     String rightDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
     String dateMore3 = LocalDate.now().plusDays(14).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
     String dateLess3 = LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
     String datePast = LocalDate.now().plusDays(-5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
     String buttonText = "Запланировать";
     CityCreater cityCreater = new CityCreater();
-/* Tests on different variants of city input */
 
+    /* Tests on different variants of city input */
     @Test
     void shouldSubmitDeliveryWhenAllFieldsRight() {
 
         String cityFromList = cityCreater.getCity();
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue(cityFromList);
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
@@ -50,15 +45,11 @@ class DeliveryCardTest {
     void shouldNotSubmitDeliveryWhenCityNotExist() {
         open("http://localhost:9999/");
         $("[data-test-id= city] input").setValue("неизвестный город");
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         String getText = $("[data-test-id='city'] span.input__sub").waitUntil(visible, 15000).getText();
@@ -69,15 +60,11 @@ class DeliveryCardTest {
     void shouldNotSubmitDeliveryWhenCityIsEmpty() {
         open("http://localhost:9999/");
         $("[data-test-id= city] input").setValue("");
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         String getText = $("[data-test-id='city'] span.input__sub").waitUntil(visible, 15000).getText();
@@ -85,7 +72,6 @@ class DeliveryCardTest {
     }
 
     /*Tests on data input*/
-
 
 
     @Test
@@ -96,17 +82,12 @@ class DeliveryCardTest {
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(dateLess3);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
-
         String getText = $(".input_invalid .input__sub").getText();
-                assertEquals("Заказ на выбранную дату невозможен", getText);
-           
+        assertEquals("Заказ на выбранную дату невозможен", getText);
 
 
     }
@@ -114,23 +95,16 @@ class DeliveryCardTest {
     @Test
     void shouldNotSubmitIfCheckboxUnSelected() {
         open("http://localhost:9999/");
-
         String cityFromList = cityCreater.getCity();
         $("[data-test-id= city] input").setValue(cityFromList);
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
-      //  $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
-
         String getText = $("[data-test-id='agreement'].input_invalid").getCssValue("color");
-      assertEquals("rgba(255, 92, 92, 1)", getText);
+        assertEquals("rgba(255, 92, 92, 1)", getText);
 
 
     }
--- src/test/java/ru/netology/DeliveryOnPhoneNumberTest.java
+++ src/test/java/ru/netology/DeliveryOnPhoneNumberTest.java
@@ -1,10 +1,12 @@
 package ru.netology;
+
 import ru.netology.CityCreater;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static com.codeborne.selenide.Condition.*;
@@ -14,7 +16,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.cssSelector;
 
 class DeliveryOnPhoneNumberTest {
-   String rightDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String rightDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     String buttonText = "Запланировать";
     CityCreater cityCreater = new CityCreater();
     /* Tests on different variants of city input */
@@ -25,80 +27,62 @@ class DeliveryOnPhoneNumberTest {
         String cityFromList = cityCreater.getCity();
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue(cityFromList);
-
-        $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+       $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+71231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         $(withText("Успешно!")).waitUntil(visible, 15000);
     }
-
-
-
 
     @Test
     void shouldNotSubmitWhenPhoneIsEmpty() {
         String cityFromList = cityCreater.getCity();
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue(cityFromList);
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         String getText = $("[data-test-id='phone'] span.input__sub").getText();
         assertEquals("Поле обязательно для заполнения", getText.trim());
     }
-//тест не проходит так как это не проверяется программой
+
+    //тест не проходит так как это не проверяется программой
     @Test
     void shouldNotSubmitWhenPhoneNumberLess11() {
         String cityFromList = cityCreater.getCity();
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue(cityFromList);
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+34123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
         String getText = $("[data-test-id='phone'] span.input__sub").getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", getText.trim());
     }
 
-     //если вводим неверный номер телефона например +09998283231 то не должне проходить, так как после
+    //если вводим неверный номер телефона например +09998283231 то не должне проходить, так как после
     //+ должна быть 7, но проходит
     @Test
     void shouldNotSubmitWhenPhoneNotStartFromPlus7() {
         String cityFromList = cityCreater.getCity();
         open("http://localhost:9999");
         $("[data-test-id= city] input").setValue(cityFromList);
-
         $(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
         $(cssSelector("[data-test-id=date] input")).sendKeys(rightDate);
-
         $(By.xpath("//span[@data-test-id='name']/descendant::input[@class='input__control']")).sendKeys("Василий Пупкин");
-
         $("[data-test-id=phone] input").sendKeys("+01231234123");
-
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText(buttonText)).click();
-  //      $(withText("Успешно!")).waitUntil(visible, 15000);
         String getText = $("[data-test-id='phone'] span.input__sub").getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", getText);
     }
